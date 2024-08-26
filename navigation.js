47dcd0a411394a25a2494ae3ada457cfdee251af
kalamado <mateus.cezario.barreto@gmail.com>
2024-08-26 17:09:23 +0000

    Improvements on navigation

---
+++ navigation.js
@@ -0,0 +1,98 @@
+const htmlBody = document.querySelector("body");
+
+const buttonShowSectionIndex = document.getElementById("toggleSectionIndex");
+buttonShowSectionIndex.addEventListener("click", toggleSectionIndex);
+
+const buttonGoTop = document.getElementById("goTopButton");
+buttonGoTop.addEventListener("click", goTop);
+
+const sectionIndex = document.getElementById("sectionIndex");
+sectionIndex.style.setProperty("display", "none");
+
+
+function goTop ()
+{
+	document.querySelector("header").scrollIntoView();
+	return;
+}
+
+
+function generateSectionIndex ()
+{
+	const headings = document.querySelectorAll("h1");
+
+
+	for ( let i=0; i < headings.length; i++ )
+	{
+		headings[i].setAttribute("id", "head" + i);
+		
+		let section = document.createElement("p");
+
+		let anchor = document.createElement("a");
+		
+		anchor.setAttribute("href", "#" + headings[i].getAttribute("id"));
+
+		anchor.textContent = headings[i].textContent;
+
+		anchor.addEventListener("click", scrollToHeading);
+
+		section.appendChild(anchor);
+		sectionIndex.appendChild(section);
+	}
+	
+	
+	return;
+}
+
+
+function toggleSectionIndex ()
+{
+	if ( sectionIndex.style.display === "none" )
+	{
+		document.querySelector("main").style.display = "none";
+				
+		sectionIndex.style.display = "grid";
+	}
+	
+
+	else
+	{
+		document.querySelector("main").style.display = "grid";
+		
+		sectionIndex.style.display = "none";
+	}
+
+	
+	return;
+}
+
+
+/*
+
+This function is called when anchor in [section index] is clicked.
+
+On execution, [section index] is hidden and the page view is scrolled to heading associated with the anchor that was clicked.
+
+The argument [event] is used to determine the heading to where the page view will be scrolled to.
+
+*/
+function scrollToHeading ( event )
+{
+	/* Close section index if it is open */
+	if ( sectionIndex.style.display === "grid" ) { toggleSectionIndex(); }
+
+
+	/*
+	Pass the [href] attribute of anchor clicked as input to function [querySelector].
+
+	[href] is an id, like [#head1] for example, so [querySelector] returns an object [Element].
+
+	On top of this object, we use the method [scrollIntoView].
+	*/
+	document.querySelector(event.target.getAttribute("href")).scrollIntoView();
+
+	return;
+}
+
+
+generateSectionIndex();
